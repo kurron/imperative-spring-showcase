e11b2cc6dce428b784b256379fab552ffcb4c4a9
Ron Kurr <kurr@jvmguy.com>
2020-04-04 19:37:30 -0400

    Package the application as a Docker image.

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM azul/zulu-openjdk:11 AS builder
+WORKDIR /tmp
+ARG APPJAR=build/libs/*.jar
+COPY ${APPJAR} app.jar
+RUN jar -xf ./app.jar
+
+FROM azul/zulu-openjdk-alpine:11
+VOLUME /tmp
+ARG DEPENDENCY=/tmp
+COPY --from=builder ${DEPENDENCY}/BOOT-INF/lib /app/lib
+COPY --from=builder ${DEPENDENCY}/META-INF /app/META-INF
+COPY --from=builder ${DEPENDENCY}/BOOT-INF/classes /app
+ENTRYPOINT ["java","-cp","app:app/lib/*","org.kurron.imperative.ShowcaseApplication"]
---
+++ bin/package.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PROGNAME=$(basename $0)
+error_exit()
+{
+	  echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+	  exit 1
+}
+
+# use time as the tag
+UNIXTIME=$(date +%s)
+
+echo "Packaging application as a Dockerimage"
+BUILD="docker build --rm \
+                    --build-arg APPJAR=build/libs/imperative-0.0.1-SNAPSHOT.jar \
+                    --tag kurron/showcase-imperative:${UNIXTIME} \
+                    --tag kurron/showcase-imperative:latest \
+                    ."
+echo "${BUILD}"
+${BUILD} || error_exit "Unable to package application"
