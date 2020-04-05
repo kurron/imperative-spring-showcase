32339bfb403f76ee70de635475a9e10a56326725
Ron Kurr <kurr@jvmguy.com>
2020-04-05 11:52:24 -0400

    Verify that sorted field is working.

--- src/main/kotlin/org/kurron/imperative/Controllers.kt
+++ src/main/kotlin/org/kurron/imperative/Controllers.kt
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+data class Stuff( val zulu: String, val hotel: String, val alpha: String )
+
 @RestController
 @RequestMapping("/echo")
 class Echo {
 
     @GetMapping("/hello", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun hello() = mapOf( "operation" to randomHexString(), "code" to randomHexString(), "should not be sent" to "" )
+
+    @GetMapping("/sorted", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun sorted() = Stuff( randomHexString(), randomHexString(), randomHexString() )
 }
