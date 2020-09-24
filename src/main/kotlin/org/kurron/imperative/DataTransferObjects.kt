f550e9f786b7b6c682062695af438a067d088a54
Ron Kurr <kurr@jvmguy.com>
2020-09-24 14:42:58 -0400

    Restricting a configuration to specific profiles no longer makes sense to me.

--- src/main/kotlin/org/kurron/imperative/DataTransferObjects.kt
+++ src/main/kotlin/org/kurron/imperative/DataTransferObjects.kt
@@ -3,9 +3,7 @@ package org.kurron.imperative
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
-import org.springframework.context.annotation.Profile
 
-@Profile("cloud", "development")
 @ConstructorBinding
 @ConfigurationProperties(  prefix = "application")
 data class ApplicationConfiguration(var snsEndpoint: String, var snsTopic: String, var sqsEndpoint: String)
