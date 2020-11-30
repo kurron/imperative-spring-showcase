9e82cb519f0ec05bab05d559ddf251fc062278fc
Ron Kurr <kurr@jvmguy.com>
2020-11-30 15:51:35 +0000

    Move the technologies being tested into a context dedicated to the test.

--- src/main/kotlin/org/kurron/imperative/ShowcaseApplication.kt
+++ src/main/kotlin/org/kurron/imperative/ShowcaseApplication.kt
@@ -1,30 +1,12 @@
 package org.kurron.imperative
 
-import com.amazonaws.auth.AWSCredentialsProvider
-import com.amazonaws.client.builder.AwsClientBuilder
-import com.amazonaws.services.sns.AmazonSNS
-import com.amazonaws.services.sns.AmazonSNSClientBuilder
-import com.amazonaws.services.sqs.AmazonSQSAsync
-import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
-import org.elasticsearch.client.RestHighLevelClient
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.cloud.aws.messaging.config.QueueMessageHandlerFactory
-import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Profile
-import org.springframework.data.elasticsearch.client.ClientConfiguration
-import org.springframework.data.elasticsearch.client.RestClients
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate
-import org.springframework.messaging.converter.MappingJackson2MessageConverter
-import org.springframework.messaging.handler.annotation.support.PayloadMethodArgumentResolver
 import org.springframework.stereotype.Component
 import java.util.concurrent.ThreadLocalRandom
 
@@ -37,55 +19,6 @@ class ShowcaseApplication {
 	@Bean
 	fun restTemplate() = RestTemplateBuilder().build()!!
 
-	@Bean
-	fun queueMessageHandlerFactory(): QueueMessageHandlerFactory {
-		val factory = QueueMessageHandlerFactory()
-		val converter = MappingJackson2MessageConverter()
-		converter.isStrictContentTypeMatch = false
-		factory.setArgumentResolvers( listOf( PayloadMethodArgumentResolver(converter) ) )
-		return factory
-	}
-
-	@Bean
-	fun amazonSQS(configuration: ApplicationConfiguration, @Value("\${cloud.aws.region.static}") region:String, provider: AWSCredentialsProvider ): AmazonSQSAsync {
-		return AmazonSQSAsyncClientBuilder.standard()
-				                          .withEndpointConfiguration( AwsClientBuilder.EndpointConfiguration( configuration.sqsEndpoint, region ) )
-				                          .withCredentials( provider )
-				                          .build()
-	}
-
-	@Bean
-	fun messagingTemplate( sqs: AmazonSQSAsync ): QueueMessagingTemplate = QueueMessagingTemplate(sqs)
-
-	@Bean
-	fun amazonSNS(configuration: ApplicationConfiguration, @Value("\${cloud.aws.region.static}") region:String, provider: AWSCredentialsProvider ): AmazonSNS {
-		return AmazonSNSClientBuilder.standard()
-				                     .withEndpointConfiguration( AwsClientBuilder.EndpointConfiguration( configuration.snsEndpoint, region ) )
-				                     .withCredentials( provider )
-				                     .build()
-	}
-
-	@Bean
-	fun elasticsearchClient( configuration: ApplicationConfiguration ): RestHighLevelClient {
-		val restConfiguration = ClientConfiguration.builder().connectedTo( configuration.elasticsearchEndpoint ).build()
-		return RestClients.create(restConfiguration).rest()
-	}
-
-	@Bean
-	fun elasticsearch( client: RestHighLevelClient ): ElasticsearchOperations {
-        return ElasticsearchRestTemplate(client)
-	}
-
-	@Bean
-	fun notificationMessagingTemplate(sns: AmazonSNS) = NotificationMessagingTemplate(sns)
-
-	@Bean
-	@Profile("!stubbed")
-	fun outboundMessagingGateway(template: NotificationMessagingTemplate, configuration: ApplicationConfiguration) = SimpleNotificationServiceGateway(template, configuration)
-
-	@Bean
-	@Profile("stubbed")
-	fun nullOutboundMessagingGateway() = NullNotificationServiceGateway()
 }
 
 fun main(args: Array<String>) {
--- src/test/kotlin/org/kurron/imperative/ShowcaseApplicationTests.kt
+++ src/test/kotlin/org/kurron/imperative/ShowcaseApplicationTests.kt
@@ -1,8 +1,14 @@
 package org.kurron.imperative
 
+import com.amazonaws.auth.AWSCredentialsProvider
+import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.services.sns.AmazonSNS
+import com.amazonaws.services.sns.AmazonSNSClientBuilder
 import com.amazonaws.services.sqs.AmazonSQSAsync
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
 import com.amazonaws.services.sqs.model.CreateQueueResult
 import com.fasterxml.jackson.annotation.JsonProperty
+import org.elasticsearch.client.RestHighLevelClient
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -13,13 +19,24 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.cloud.aws.messaging.config.QueueMessageHandlerFactory
+import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
 import org.springframework.data.annotation.Id
+import org.springframework.data.elasticsearch.client.ClientConfiguration
+import org.springframework.data.elasticsearch.client.RestClients
 import org.springframework.data.elasticsearch.annotations.Document as ElasticsearchDocument
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates
 import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.messaging.converter.MappingJackson2MessageConverter
+import org.springframework.messaging.handler.annotation.support.PayloadMethodArgumentResolver
 import org.springframework.data.mongodb.core.mapping.Document as MongodbDocument
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.DynamicPropertyRegistry
@@ -43,7 +60,7 @@ import java.util.concurrent.ThreadLocalRandom
 @ActiveProfiles("containerized")
 class ShowcaseApplicationTests {
 
-    // set the properies required to connect to the test containers
+    // set the properties required to connect to the test containers
     @Suppress("unused")
     companion object Initializer {
         val indexCoordinates = IndexCoordinates.of( Integer.toHexString( ThreadLocalRandom.current().nextInt( Integer.MAX_VALUE ) ) )
@@ -79,6 +96,59 @@ class ShowcaseApplicationTests {
             registry.add("spring.data.mongodb.uri" ) { mongodb.replicaSetUrl }
             registry.add("application.elasticsearch-endpoint" ) { elasticsearch.httpHostAddress }
         }
+    }
+
+    @TestConfiguration
+    class ShowcaseApplicationTestsConfiguration {
+        @Bean
+        fun queueMessageHandlerFactory(): QueueMessageHandlerFactory {
+            val factory = QueueMessageHandlerFactory()
+            val converter = MappingJackson2MessageConverter()
+            converter.isStrictContentTypeMatch = false
+            factory.setArgumentResolvers( listOf( PayloadMethodArgumentResolver(converter) ) )
+            return factory
+        }
+
+        @Bean
+        fun amazonSQS(configuration: ApplicationConfiguration, @Value("\${cloud.aws.region.static}") region:String, provider: AWSCredentialsProvider): AmazonSQSAsync {
+            return AmazonSQSAsyncClientBuilder.standard()
+                    .withEndpointConfiguration( AwsClientBuilder.EndpointConfiguration( configuration.sqsEndpoint, region ) )
+                    .withCredentials( provider )
+                    .build()
+        }
+
+        @Bean
+        fun messagingTemplate( sqs: AmazonSQSAsync ): QueueMessagingTemplate = QueueMessagingTemplate(sqs)
+
+        @Bean
+        fun amazonSNS(configuration: ApplicationConfiguration, @Value("\${cloud.aws.region.static}") region:String, provider: AWSCredentialsProvider): AmazonSNS {
+            return AmazonSNSClientBuilder.standard()
+                    .withEndpointConfiguration( AwsClientBuilder.EndpointConfiguration( configuration.snsEndpoint, region ) )
+                    .withCredentials( provider )
+                    .build()
+        }
+
+        @Bean
+        fun elasticsearchClient( configuration: ApplicationConfiguration ): RestHighLevelClient {
+            val restConfiguration = ClientConfiguration.builder().connectedTo( configuration.elasticsearchEndpoint ).build()
+            return RestClients.create(restConfiguration).rest()
+        }
+
+        @Bean
+        fun elasticsearch( client: RestHighLevelClient): ElasticsearchOperations {
+            return ElasticsearchRestTemplate(client)
+        }
+
+        @Bean
+        fun notificationMessagingTemplate(sns: AmazonSNS) = NotificationMessagingTemplate(sns)
+
+        @Bean
+        @Profile("!stubbed")
+        fun outboundMessagingGateway(template: NotificationMessagingTemplate, configuration: ApplicationConfiguration) = SimpleNotificationServiceGateway(template, configuration)
+
+        @Bean
+        @Profile("stubbed")
+        fun nullOutboundMessagingGateway() = NullNotificationServiceGateway()
     }
 
     @Autowired
