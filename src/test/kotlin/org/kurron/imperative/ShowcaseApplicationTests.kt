c03cb4d933642d63e4bac6ff7a4979480676f074
Ron Kurr <kurr@jvmguy.com>
2021-04-16 18:53:14 +0000

    Implementing tips from https://phauer.com/2018/best-practices-unit-testing-kotlin/

--- src/test/kotlin/org/kurron/imperative/ShowcaseApplicationTests.kt
+++ src/test/kotlin/org/kurron/imperative/ShowcaseApplicationTests.kt
@@ -10,11 +10,15 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.junit.jupiter.Testcontainers
+import java.time.Duration
 
-@SpringBootTest(classes = [ApplicationConfiguration::class], webEnvironment = SpringBootTest.WebEnvironment.NONE )
+@SpringBootTest(classes = [ApplicationConfiguration::class], webEnvironment = SpringBootTest.WebEnvironment.NONE, properties = ["showcase.inner.delta=24h"] )
+@Import(ShowcaseApplicationTests.ShowcaseApplicationTestsConfiguration::class)
 @Testcontainers(disabledWithoutDocker = true)
 class ShowcaseApplicationTests {
 
@@ -28,7 +32,9 @@ class ShowcaseApplicationTests {
     }
 
     @TestConfiguration
-    class ShowcaseApplicationTestsConfiguration {
+    class ShowcaseApplicationTestsConfiguration(private val configuration: BravoProperties) {
+        @Bean
+        fun someDuration(): Duration = configuration.delta
     }
 
     @Autowired
@@ -36,6 +42,9 @@ class ShowcaseApplicationTests {
 
     @Autowired
     lateinit var inner: BravoProperties
+
+    @Autowired
+    lateinit var duration: Duration
 
     val logger = LoggerFactory.getLogger(ShowcaseApplicationTests::class.java)
 
@@ -71,6 +80,9 @@ class ShowcaseApplicationTests {
     fun applicationStarts() {
         println("outer: $outer")
         println("inner: $inner")
+        Assertions.assertNotNull(duration, "Durations wasn't injected!")
+        println("duration: $duration")
+        Assertions.assertEquals(Duration.ofHours(24), duration, "Property was not overridden by the test!")
     }
 
     @Suppress("unused")
